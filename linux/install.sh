ac84259f0e9b77faf50e1836902ec82c3ea8147e
bruce wayne <david@dimocom.com>
2015-01-13 22:32:45 +0100

    added linux bash

---
+++ linux/install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+if [ ! -d ~/.drumcommit ]; then
+mkdir ~/.drumcommit
+fi
+if [ ! -f ~/.drumcommit/drumroll.wav ]; then
+cp ../drumroll.wav ~/.drumcommit/
+fi
+if [ ! -f $1/.git/hooks/pre-commit ]; then
+echo "" >> $1/.git/hooks/pre-commit
+fi
+
+if ! grep -Fxq "aplay ~/.drumcommit/drumroll.wav" $1/.git/hooks/pre-commit
+then
+echo "### drumcommit ###" >> $1/.git/hooks/pre-commit
+echo "aplay ~/.drumcommit/drumroll.wav" >> $1/.git/hooks/pre-commit
+echo "### end drumcommit ###" >> $1/.git/hooks/pre-commit
+chmod 755 $1/.git/hooks/pre-commit
+fi
+
+exit 0
+
---
+++ mac/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+if [ ! -d ~/.drumcommit ]; then
+mkdir ~/.drumcommit
+fi
+if [ ! -f ~/.drumcommit/drumroll.wav ]; then
+cp ../drumroll.wav ~/.drumcommit/
+fi
+
+if ! grep -Fxq "afplay ~/.drumcommit/drumroll.wav" $1/.git/hooks/pre-commit
+then
+echo "### drumcommit ###" >> $1/.git/hooks/pre-commit
+echo "afplay ~/.drumcommit/drumroll.wav" >> $1/.git/hooks/pre-commit
+echo "### end drumcommit ###" >> $1/.git/hooks/pre-commit
+chmod 755 $1/.git/hooks/pre-commit
+fi
+
+exit 0
+

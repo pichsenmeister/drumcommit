07e2371a670052bfc65819f41f81bd7c94fa4912
bruce wayne <david@dimocom.com>
2015-01-14 10:19:31 +0100

    fixed mac script

--- mac/install.sh
+++ mac/install.sh
@@ -5,6 +5,9 @@ fi
 if [ ! -f ~/.drumcommit/drumroll.wav ]; then
 cp ../drumroll.wav ~/.drumcommit/
 fi
+if [ ! -f $1/.git/hooks/pre-commit ]; then
+echo "" >> $1/.git/hooks/pre-commit
+fi
 
 if ! grep -Fxq "afplay ~/.drumcommit/drumroll.wav" $1/.git/hooks/pre-commit
 then
